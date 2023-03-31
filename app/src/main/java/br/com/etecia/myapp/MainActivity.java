61778557705ea5b2ba40df97030ea4afbe963e60
GustavoVFA <gustavovfamorim@gmail.com>
2023-03-30 21:37:02 -0300

    definindo XMl no jav ( - configuração do menu pelo java - setando ids de itens do menu - configuração dos itens do menu )

--- app/src/main/java/br/com/etecia/myapp/MainActivity.java
+++ app/src/main/java/br/com/etecia/myapp/MainActivity.java
@@ -4,11 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.material.appbar.MaterialToolbar;
+
 public class MainActivity extends AppCompatActivity {
+    //declarando variavel global que ira representar o xml no java
+
+    MaterialToolbar idToolBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Apresentando o xml para o java
+        idToolBar = findViewById(R.id.idToolBar);
     }
 }
